9ce0a33f06badc9ba2cc09f1b3c01cc0d631a5f4
LiCHH <zjuchli95@gmail.com>
2019-01-24 20:47:50 +0800

    add a path

--- src/nav_points.cpp
+++ src/nav_points.cpp
@@ -17,23 +17,28 @@ int main(int argc, char** argv)
   ros::NodeHandle nh("~");
 
   int num_points;
-  vector<double> x_coords, y_coords, theta_coords;
+  vector<double> x_coords, y_coords, qz_coords, qw_coords;
   nh.getParam("num_points", num_points);
   nh.getParam("points_x", x_coords);
   nh.getParam("points_y", y_coords);
-  nh.getParam("points_theta", theta_coords);
+  nh.getParam("points_qz", qz_coords);
+  nh.getParam("points_qw", qw_coords);
   ROS_INFO("get param %f", x_coords[0]);
-  if(x_coords.size() != num_points || y_coords.size() != num_points || theta_coords.size() != num_points){
+  if(x_coords.size() != num_points || y_coords.size() != num_points || qz_coords.size() != num_points){
     ROS_ERROR("Wrong Point Number!");
     return 0;
   }
   
-  vector<geometry_msgs::Pose2D> goal_poses;
-  geometry_msgs::Pose2D tmp_pose;
+  vector<geometry_msgs::Pose> goal_poses;
+  geometry_msgs::Pose tmp_pose;
   for(auto i = 0; i < num_points; ++i){
-    tmp_pose.x = x_coords[i];
-    tmp_pose.y = y_coords[i];
-    tmp_pose.theta = theta_coords[i];
+    tmp_pose.position.x = x_coords[i];
+    tmp_pose.position.y = y_coords[i];
+    tmp_pose.position.z = 0;
+    tmp_pose.orientation.w = qw_coords[i];
+    tmp_pose.orientation.z = qz_coords[i];
+    tmp_pose.orientation.y = 0;
+    tmp_pose.orientation.x = 0;
     goal_poses.push_back(tmp_pose);
   }
 
@@ -44,22 +49,22 @@ int main(int argc, char** argv)
   }
 
   MoveBaseGoal curr_goal;
-  curr_goal.target_pose.header.frame_id = "base_link";
+  curr_goal.target_pose.header.frame_id = "/map";
 
-  geometry_msgs::Pose2D curr_pose;
+  geometry_msgs::Pose curr_pose;
   size_t point_states = 0;
 
   tf::Quaternion q;
   while(point_states < num_points){
     curr_pose = goal_poses[point_states];
     curr_goal.target_pose.header.stamp = ros::Time::now();
-    curr_goal.target_pose.pose.position.x = curr_pose.x;
-    curr_goal.target_pose.pose.position.y = curr_pose.y;
-    q.setRPY(0, 0, curr_pose.theta);
-    curr_goal.target_pose.pose.orientation.w = q.w();
-    curr_goal.target_pose.pose.orientation.x = q.x();
-    curr_goal.target_pose.pose.orientation.y = q.y();
-    curr_goal.target_pose.pose.orientation.z = q.z();
+    curr_goal.target_pose.pose.position.x = curr_pose.position.x;
+    curr_goal.target_pose.pose.position.y = curr_pose.position.y;
+    curr_goal.target_pose.pose.position.y = curr_pose.position.z;
+    curr_goal.target_pose.pose.orientation.w = curr_pose.orientation.w;
+    curr_goal.target_pose.pose.orientation.x = curr_pose.orientation.x;
+    curr_goal.target_pose.pose.orientation.y = curr_pose.orientation.y;
+    curr_goal.target_pose.pose.orientation.z = curr_pose.orientation.z;
 
     ac.sendGoal(curr_goal);
     ac.waitForServer();
